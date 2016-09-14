f45505e6f32caa15de88e61ad59b669e0e3c10d7
Kyle Braver <kyle.braver@gmail.com>
2016-09-14 15:11:44 -0700

    Implement AI functionality.

---
+++ game.rb
@@ -0,0 +1,10 @@
+require "./game_board.rb"
+
+
+
+
+def display_menu
+	puts "menu: display menu"
+	puts "move: choose next move"
+	puts "instructions: read game instructions"
+end
--- game_board.rb
+++ game_board.rb
@@ -1,6 +1,10 @@
 class GameBoard
   
-  def initialize
+  def initialize(player_mark="X")
+    @player_mark = player_mark
+    @player_mark = "O" unless @player_mark.eql?("X")
+    player_mark.eql?("X") ? @ai_mark = "O" : @ai_mark = "X"
+
     @move_log = {
       :top_left => " ",
       :top_middle => " ",
@@ -9,37 +13,117 @@ class GameBoard
       :middle_middle => " ",
       :middle_right => " ",
       :bottom_left => " ",
-      :botton_middle => " ",
-      :botton_right => " "}
+      :bottom_middle => " ",
+      :bottom_right => " "} 
+
+    @win_lanes = {
+      :top_row => [:top_left, :top_middle, :top_right],
+      :middle_row => [:middle_left, :middle_middle, :middle_right],
+      :bottom_row => [:bottom_left, :bottom_middle, :bottom_right],
+      :left_column => [:top_left, :middle_left, :bottom_left],
+      :middle_column => [:top_middle, :middle_middle, :bottom_middle],
+      :right_column => [:top_right, :middle_right, :bottom_right],
+      :left_diagonal => [:top_left, :middle_middle, :bottom_right],
+      :right_diagonal => [:top_right, :middle_middle, :bottom_left]}
+
+    @board_state = {
+      :top_row => :neutral,
+      :middle_row => :neutral,
+      :bottom_row => :neutral,
+      :left_column => :neutral,
+      :middle_column => :neutral,
+      :right_column => :neutral,
+      :left_diagonal => :neutral,
+      :right_diagonal => :neutral}
   end
 
-  def updateGameBoard(location, move)
+  private
+
+  #updates @board_state to reflect current conditions of board
+  def analyze_board
+    @win_lanes.each do |key, win_lane|
+      ai_count = 0
+      player_count = 0
+
+      win_lane.each do |square|
+        ai_count += 1 if @move_log[square].eql?(@ai_mark)
+        player_count += 1 if @move_log[square].eql?(@player_mark)
+      end
+
+      if(player_count == 2)
+        @board_state[key] = :lean_player
+      elsif(ai_count == 2)
+        @board_state[key] = :lean_ai
+      elsif(player_count == 3)
+        @board_state[key] = :win_player
+      elsif(ai_count == 3)
+        @board_state[key] = :win_ai
+      else
+        @board_state[key] = :neutral
+      end
+    end
+  end
+
+  #if game can be won on next turn, return that the location of that square immediately
+  #else return the location of a square where there is threat of loss
+  #if no win lane is in threat of loss return nil 
+  def prioritize_moveset
+    analyze_board
+    threat_of_loss = nil
+
+    @board_state.each do |location, state|
+      return find_empty_square(location) if state.eql?(:lean_ai)
+      threat_of_loss = location if state.eql?(:lean_player)
+    end
+    
+    if threat_of_loss.nil?
+      return nil
+    else
+      return find_empty_square(threat_of_loss)
+    end
+  end
+
+  #given a win_lane, this method will find its first empty square (i.e. " ")
+  def find_empty_square(win_lane)
+    @win_lanes[win_lane].each do |square|
+      return square if @move_log[square].eql?(" ")
+    end
+  end
+
+  #updates the board to reflect a given move
+  def update_game_board(location, move)
     @move_log[location] = move
   end
 
+  #public methods below
   public
   
-  def recordPlayerMove(location, move)
+  def record_player_move(location, move)
     return false if(!@move_log.keys.include?(location) || !move.match(/[XxOo]/))
     
-    updateGameBoard(location, move)
+    update_game_board(location, move.upcase)
     return true
   end
 
-  def displayBoard
+  #prints a summary of @game_state to screen
+  def display_board_state
+    analyze_board
+    @board_state.each {|key, value| puts "#{key}: #{value}"}
+  end
+
+  #prints game board to screen
+  def display_board
     puts ""
     puts "  #{@move_log[:top_left]} | #{@move_log[:top_middle]} | #{@move_log[:top_right]} "
     puts "-------------"
     puts "  #{@move_log[:middle_left]} | #{@move_log[:middle_middle]} | #{@move_log[:middle_right]} "
     puts "-------------"
-    puts "  #{@move_log[:bottom_left]} | #{@move_log[:botton_middle]} | #{@move_log[:botton_right]} "
+    puts "  #{@move_log[:bottom_left]} | #{@move_log[:bottom_middle]} | #{@move_log[:bottom_right]} "
     puts ""
   end
-end
 
-a = GameBoard.new
-a.displayBoard
-p a.recordPlayerMove(:top_left, "X")
-p a.recordPlayerMove("blue", "X")
-p a.recordPlayerMove(:top_left, "blue")
-a.displayBoard
+  #ai takes its turn when this method is called
+  def ai_move
+    update_game_board(prioritize_moveset, @ai_mark)
+  end
+end
